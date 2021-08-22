bc5840d42f3f58281516bf0cbfb2f94a0b2a02b0
Mr.Beast <ahmadalzoubi13579@gmail.com>
2021-08-23 01:26:56 +0300

    change the input

--- yolo.py
+++ yolo.py
@@ -205,9 +205,9 @@ class YOLO(object):
 
 def detect_video(yolo, video_path, output_path=""):
     import cv2
-    # vid = cv2.VideoCapture(video_path)
+    vid = cv2.VideoCapture(video_path)
     # for webcame
-    vid = cv2.VideoCapture(0)
+    # vid = cv2.VideoCapture(0)
     if not vid.isOpened():
         raise IOError("Couldn't open webcam or video")
     video_FourCC    = int(vid.get(cv2.CAP_PROP_FOURCC))
